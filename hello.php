bc3a298c38cf8c904ee98493674cd32e8137b5d0
lulumuksin <lulumksn@gmail.com>
2021-02-24 23:18:09 +0700

    php_dasar

---
+++ hello.php
@@ -0,0 +1,17 @@
+<?php
+define("PHI",3.14);
+$jari_jari =7;
+$luas_lingkaran = PHI * $jari_jari * $jari_jari;
+echo $luas_lingkaran;
+
+$angka = 10;
+$nama = 'kawai ruuchan'; 
+$angka_desimal = 3.5;
+echo $angka;
+echo "</br>";
+echo $nama;
+echo "</br>";
+echo $angka_desimal;
+echo "</br>";
+echo "dokumen saya : ".$_SERVER["DOCUMENT_ROOT"];
+?>
